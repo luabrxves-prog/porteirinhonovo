78b78e9c2a80e12dc24e065755b6d4692b213629
Lu <luabrxves@gmail.com>
2026-09-09 20:42:09 -0300

    feat: expose shared remote sync client

--- porteirinho-main/app/src/main/java/br/com/porteirinho/AppContainer.kt
+++ porteirinho-main/app/src/main/java/br/com/porteirinho/AppContainer.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import br.com.porteirinho.data.PatrolRepository
 import br.com.porteirinho.data.local.AppDatabase
 import br.com.porteirinho.domain.DeviceIdentity
+import br.com.porteirinho.sync.RemoteSyncClient
 
 class AppContainer(context: Context) {
     val database: AppDatabase = AppDatabase.create(context)
-    val repository: PatrolRepository = PatrolRepository(database, DeviceIdentity(context))
+    val deviceIdentity = DeviceIdentity(context)
+    val repository: PatrolRepository = PatrolRepository(database, deviceIdentity)
+    val remoteSyncClient: RemoteSyncClient = RemoteSyncClient(context, database, deviceIdentity)
 }
