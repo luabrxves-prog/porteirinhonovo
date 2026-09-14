df9316ece10e42acb6cd9fad937a7f595e6e3e18
Lu <luabrxves@gmail.com>
2026-09-14 10:51:46 -0300

    feat: wire admin configuration repository

--- porteirinho-main/app/src/main/java/br/com/porteirinho/AppContainer.kt
+++ porteirinho-main/app/src/main/java/br/com/porteirinho/AppContainer.kt
@@ -1,6 +1,7 @@
 package br.com.porteirinho
 
 import android.content.Context
+import br.com.porteirinho.data.AdminConfigurationRepository
 import br.com.porteirinho.data.PatrolRepository
 import br.com.porteirinho.data.local.AppDatabase
 import br.com.porteirinho.domain.DeviceIdentity
@@ -8,4 +9,5 @@ import br.com.porteirinho.domain.DeviceIdentity
 class AppContainer(context: Context) {
     val database: AppDatabase = AppDatabase.create(context)
     val repository: PatrolRepository = PatrolRepository(database, DeviceIdentity(context))
+    val adminRepository: AdminConfigurationRepository = AdminConfigurationRepository(context, database)
 }
